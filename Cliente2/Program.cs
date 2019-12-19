53bcd3e2eba625c744aa1aa760372fcf3a0e7f54
felipegalmeida <felipegalmeida@outlook.com>
2019-12-19 11:34:29 -0300

    Cliente 2 first endpoint implementation

--- Cliente2/Program.cs
+++ Cliente2/Program.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
-using Cliente2.EstoqueService;
+using Cliente2.EstoqueService;
+
 
 namespace Cliente2 {
     class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("Press ENTER when the service has started");
+            Console.ReadLine();
+
             // Create a proxy object and connect to the service
-            EstoqueServiceClient proxy = new EstoqueServiceClient();
+            EstoqueServiceClient proxy = new EstoqueServiceClient("BasicHttpBinding_IServicoEstoque");
                 
             // Test the operations in the service
             Console.WriteLine("Cliente 2");
