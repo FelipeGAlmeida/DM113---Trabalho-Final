182a263b4b2dfbb475081bc368506f5ebcc62e88
felipegalmeida <felipegalmeida@outlook.com>
2019-12-18 23:57:04 -0300

    First Host Implementation

---
+++ EstoqueServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServicoEstoque;
+
+namespace EstoqueServiceHost {
+    class Program {
+        static void Main(string[] args) {
+
+            ServiceHost productsServiceHost = new ServiceHost(typeof(EstoqueService));
+            productsServiceHost.Open();
+            Console.WriteLine("Service Running");
+            Console.ReadLine();
+            Console.WriteLine("Service Stopping");
+            productsServiceHost.Close();
+
+        }
+    }
+}
